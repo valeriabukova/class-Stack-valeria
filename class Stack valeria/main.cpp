452b9e48aeef7d639d51be2375c4115f8e090ce0
valeriabukova <valeriasergeevna688@gmail.com>
2024-10-30 13:13:52 +0200

    class Stack

--- class Stack valeria/main.cpp
+++ class Stack valeria/main.cpp
@@ -7,8 +7,90 @@
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+template <typename T>
+class Stack {
+private:
+    struct Node {
+        T data;
+        Node* next;
+
+        Node(T value) : data(value), next(nullptr) {}
+    };
+
+    Node* topNode;
+    size_t stackSize;
+
+public:
+    
+    Stack() : topNode(nullptr), stackSize(0) {}
+
+    // Деструктор
+    ~Stack() {
+        while (!isEmpty()) {
+            pop();
+        }
+    }
+
+   
+    void push(T value) {
+        Node* newNode = new Node(value);
+        newNode->next = topNode;
+        topNode = newNode;
+        ++stackSize;
+    }
+
+  
+    void pop() {
+        if (isEmpty()) {
+            std::cout << "Ошибка: Стек пустой, невозможно удалить элемент.\n";
+            return;
+        }
+        Node* temp = topNode;
+        topNode = topNode->next;
+        delete temp;
+        --stackSize;
+    }
+
+    
+    T top() const {
+        if (isEmpty()) {
+            std::cout << "Ошибка: Стек пустой.\n";
+            return T();
+        }
+        return topNode->data;
+    }
+
+    
+    bool isEmpty() const {
+        return topNode == nullptr;
+    }
+
+   
+    size_t size() const {
+        return stackSize;
+    }
+};
+
+int main() {
+    Stack<int> stack;
+
+   
+    stack.push(10);
+    stack.push(20);
+    stack.push(30);
+
+    std::cout << "Верхний элемент: " << stack.top() << std::endl;
+    std::cout << "Размер стека: " << stack.size() << std::endl;
+
+   
+    stack.pop();
+    std::cout << "Верхний элемент после удаления: " << stack.top() << std::endl;
+    std::cout << "Размер стека после удаления: " << stack.size() << std::endl;
+
+    
+    stack.pop();
+    stack.pop();
+    stack.pop();
+
     return 0;
 }
